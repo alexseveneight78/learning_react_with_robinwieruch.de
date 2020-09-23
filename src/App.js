be6e9034532b6f49882b3b86628b035ba5dd1931
alexseveneight <alexseveneight@gmail.com>
2020-09-23 07:34:08 +0300

    standalone List coponent

--- src/App.js
+++ src/App.js
@@ -20,21 +20,29 @@ const list = [
   },
 ];
 
+function List(){
+  return list.map(item => {
+    return (
+      <div key={item.objectID}>
+        <span>
+          <a href={item.url}>{item.title}</a>
+        </span>
+        <span>{item.author}</span>
+        <span>{item.num_comments}</span>
+        <span>{item.points}</span>      
+      </div>
+    )
+  })
+}
+
+
+
 function App() {
   return (
     <div className="App">
-      {list.map(function(item) {
-        return (
-          <div key={item.objectID}>
-            <span>
-              <a href={item.url}>{item.title}</a>
-            </span>
-            <span>{item.author}</span>
-            <span>{item.num_comments}</span>
-            <span>{item.points}</span>
-          </div>
-        );
-      })}
+      Hello
+      <hr/>
+      <List />
     </div>
   );
 }
